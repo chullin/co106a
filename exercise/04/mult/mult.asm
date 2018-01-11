bac66e7f0cd6c108563419c650a1cf7e721e4864
chullin <fg6ts15@gmail.com>
2018-01-11 09:55:53 +0800

    finish

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// R0=3
+//  @3   // 0
+//	D=A  // 1
+//	@R0  // 2
+//	M=D  // 3
+	
+// R1=5
+//	@5   // 4
+//	D=A  // 5
+//	@R1  // 6
+//	M=D  // 7
+
+
+	@R2           // R2 = 0
+	M = 0  
+	
+	@R0           // a = R0
+	D = M  
+	@a     
+	M = D  
+(LOOP)
+
+	@a            //   if (a <= 0) goto EXIT
+	D = M  
+	@EXIT  
+	D; JLE        //   JLE = Jump Less Equal  JGE
+	
+	@a            //   a=a-1;
+	M = M-1  
+
+	@R1           // 	R2 = R2 + R1;
+	D = M
+	@R2   
+	M = D+M 
+
+	@LOOP         // 	goto LOOP
+  0; JMP 
+(EXIT)
+  @EXIT  
+	0; JMP 
